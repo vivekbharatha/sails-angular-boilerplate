e3a746f205bc9778a791be2469a6e5ea95d1b38e
Vivek Bharatha <vivek.bharatha@gmail.com>
2015-09-17 23:13:31 +0530

    New Baby!

---
+++ assets/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name assetsApp
+ * @description
+ * # assetsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yourApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeController'
+      });
+  });
---
+++ assets/scripts/controllers/HomeController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name assetsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the assetsApp
+ */
+angular.module('yourApp')
+  .controller('HomeController', function ($scope) {
+    $scope.geek = 'Geeky Buddy!';
+  });
